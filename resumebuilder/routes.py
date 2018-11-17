7bb82c0ae4b1517d168770f8ba0c3c053edac808
Gabriel Rodriguez <gabriel.rodriguez66@upr.edu>
2018-11-17 19:04:40 -0400

    Allowed creation of Experience entity in database with proper
    field validators. Implemented the corresponding form for experience
    registration, added experience registration route and added the option
    in the nav bar.

--- resumebuilder/routes.py
+++ resumebuilder/routes.py
@@ -1,15 +1,15 @@
 from flask import url_for, redirect, flash, render_template
 from flask_login import current_user, login_user, logout_user
 from resumebuilder import app, db, bcrypt
-from resumebuilder.forms import RegistrationForm, LoginForm
-from resumebuilder.models import User
+from resumebuilder.forms import RegistrationForm, LoginForm, ExperienceForm
+from resumebuilder.models import User, Experience, Tag
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -44,3 +44,30 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/ExperienceRegistration', methods=['GET', 'POST'])
+def registerExperience():
+    form = ExperienceForm()
+
+    if form.validate_on_submit():
+        experience = Experience(proposition1=form.proposition1.data, proposition2=form.proposition2.data, proposition3=
+        form.proposition3.data, date=form.date.data, role=form.role.data, host=form.host.data, user_id=current_user.id)
+        db.session.add(experience)
+
+        experience_tags = form.tags.data.split()
+        tags_qs = Tag.query.all()
+        db_tags = [tag.name for tag in tags_qs]
+        for tag in experience_tags:
+            if tag not in db_tags:
+                temp_Tag = Tag(name=tag)
+                db.session.add(temp_Tag)
+            else:
+                temp_Tag = list(filter(lambda x: x.name == tag, tags_qs))[0]
+            experience.tags.append(temp_Tag)
+
+        db.session.commit()
+        flash("Your experience has been registered successfully", 'success')
+        return redirect(url_for('index'))
+
+    return render_template('register_experiences.html', title='Register Experience', form=form)
